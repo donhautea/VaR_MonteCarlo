d3a84edb4a657c5ced7103d028a1111e35a3d345
Adonis Hautea <donhautea@gmail.com>
2024-08-08 18:46:30 +0800

    VaR_MonteCarlo

--- VaR_MonteCarlo_App.py
+++ VaR_MonteCarlo_App.py
@@ -100,7 +100,7 @@ st.pyplot(fig)
 # Analysis of VaR
 st.write("## Analysis of VaR at Different Confidence Levels")
 for level, var in adjusted_portfolio_VaRs.items():
-    st.write(f"At the {int(level*100)}% confidence level, the Value at Risk (VaR) is {var:,.0f}. This means that there is a {int(level*100)}% chance that the portfolio will not lose more than {var:,.0f} in a single day. Conversely, there is a {100 - int(level*100)}% chance that the portfolio will lose more than {var:,.0f} in a single day.")
+    st.write(f"At the {int(level*100)}% confidence level, the Value at Risk (VaR) is {portfolio_VaRs[level]*100:,.3f}%. This means that there is a {int(level*100)}% chance that the portfolio will not lose more than {var:,.0f} in a single day. Conversely, there is a {100 - int(level*100)}% chance that the portfolio will lose more than {var:,.0f} in a single day.")
 
 # Display DataFrames to ensure they are read correctly and new columns are added
 st.write("## Closing Prices DataFrame")
